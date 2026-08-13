d91a4c82ad344ff6dfbf2aa49f9d2c8349bc0614
txnas <txnas@users.noreply.gitee.com>
2026-08-13 20:25:36 +0800

    sync tvbox alias and output url

--- update-tvbox-json.ps1
+++ update-tvbox-json.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Stop"
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
 Set-Location $PSScriptRoot
@@ -6,4 +6,4 @@ python .\update_tvbox_config.py
 python -m json.tool .\jsm.json > $null
 node --check ".\js\xiaoman-douyin.js"
 Write-Host "OK: 已更新合并源 $PSScriptRoot\jsm.json"
-Write-Host "导入地址: https://gitee.com/txnas/lufeitv/raw/master/jsm.json"
+Write-Host "导入地址: https://raw.githubusercontent.com/520pt/lufeitv-tvbox/master/jsm.json"
